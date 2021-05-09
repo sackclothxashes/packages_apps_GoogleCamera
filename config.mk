7b2e4eb3d6e211a526b74923563c9acc0905ee39
sackclothxashes <11472957+George140201@users.noreply.github.com>
2021-05-09 07:45:00 +0000

    Adding Gcam

---
+++ config.mk
@@ -0,0 +1,6 @@
+# GoogleCamera
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/privapp-permissions-googlecamera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-googlecamera.xml
